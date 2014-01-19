78f2caaf245bf674206c751475ae5439ad50d362
Chrisopher Lubaway <asimplechris@gmail.com>
2014-01-19 08:49:06 -0800

    Added basic folder layout and hello world views

--- app.rb
+++ app.rb
@@ -1,11 +1,8 @@
 require 'sinatra'
 
-enable :sessions
-use Rack::Flash
-
 $LOAD_PATH.unshift(File.expand_path('.'))
 
-['models', 'routes'].each do |directory_to_preload|
+['routes', 'initializers'].each do |directory_to_preload|
   Dir["#{directory_to_preload}/*.rb"].each do |file_to_load|
     require file_to_load
   end
